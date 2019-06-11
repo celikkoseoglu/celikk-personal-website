882b330a07299bd496f80df3e3f8ca30b5921d11
Çelik Köseoğlu <celikkoseoglu@Celiks-MacBook-Pro.local>
2019-06-11 23:27:04 +0100

    added profile picture and some other stuff from OG personal website

--- src/index.js
+++ src/index.js
@@ -10,6 +10,25 @@ const skills = require('./data/skills');
 
 ReactDOM.render(
     <div className="container">
+
+        <div className="row">
+            <div className="col-md-4">
+                <img className="col-md-12" src="https://avatars3.githubusercontent.com/u/5185809?s=460&v=4"
+                     alt="My profile picture from github"/>
+            </div>
+            <div className="col-md-4">
+                <h4>Celik Koseoglu</h4>
+                <h5>Full Stack Developer</h5>
+            </div>
+        </div>
+
+        <div className="col-md-4">
+            <h3>Why a Developer?</h3>
+            <p>Computers have always fascinated me. I was always on the curious side and wanted to learn how they
+                operated. One day I went to the bookstore and walked out with a programming language book. Now I am
+                studying computer science and doing internships in cool companies.</p>
+        </div>
+
         <div className="row">
 
             <div className="col-md-4">
