ba2a336c60d487535b08aef2e01696c569cbf35a
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-07-09 23:16:50 +0100

    made experience and titleAndText components eslint compliant

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,18 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import BulletPoint from "./bulletpoint";
 
-class Experience extends Component {
-  render() {
-    return (
-      <div>
-        <h5> {this.props.companyName} </h5>
-        <p> {this.props.description} </p>
-        {this.props.bulletPoints.map(bulletPoint => (
-          <BulletPoint bulletPoint={bulletPoint} />
-        ))}
-      </div>
-    );
-  }
-}
+const Experience = ({ companyName, description, bulletPoints }) => {
+  return (
+    <div>
+      <h5> {companyName} </h5>
+      <p> {description} </p>
+      {bulletPoints.map(bulletPoint => (
+        <BulletPoint bulletPoint={bulletPoint} />
+      ))}
+    </div>
+  );
+};
+
+Experience.propTypes = {
+  companyName: PropTypes.string,
+  description: PropTypes.string,
+  bulletPoints: PropTypes.arrayOf(PropTypes.string)
+};
+
+Experience.defaultProps = {
+  companyName: null,
+  description: null,
+  bulletPoints: null
+};
 
 export default Experience;
