b60403e964f86705d75fbfc369f843968a288441
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-07-15 23:46:46 +0100

    trying to replicate footer from the OG website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../stylesheets/font-awesome.css";
+
+const Footer = () => {
+  return (
+    <footer className="text-center col-md-12">
+      <div className="row">
+        <div className="col-md-4">
+          <span>Çelik Köseoğlu</span>
+          <p className="text-muted">
+            Designed in co-op with Morvaksu
+          </p>
+        </div>
+        <div className="col-md-8">
+          <ul className="list-inline">
+            <li>
+              <p>a</p>
+            </li>
+            <li>
+              <p>a</p>
+            </li>
+            <li>
+              <p>a</p>
+            </li>
+            <li>
+              <p>a</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
