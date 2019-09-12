5448a3066995ded895d897ff436da6367e329daa
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-09-12 22:08:44 +0100

    formatting

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { paddingDefault, titleFontSize } from "../stylesheets/components/Heading.module.sass";
+import {
+  paddingDefault,
+  titleFontSize
+} from "../stylesheets/components/Heading.module.sass";
 
 const Heading = ({ text }) => {
-  return <h2 className={`d-block text-center ${paddingDefault} ${titleFontSize}`}>{text}</h2>;
+  return (
+    <h2 className={`d-block text-center ${paddingDefault} ${titleFontSize}`}>
+      {text}
+    </h2>
+  );
 };
 
 Heading.propTypes = {
