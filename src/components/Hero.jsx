e1e3da5f465734fbcd194d769b4e75724dc8ffc5
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-10-28 19:04:12 +0000

    ported hero section with desktop styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,86 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
 
-import Button from "@material-ui/core/Button";
+import {
+  intro,
+  introMargin,
+  introHeadingStyle,
+  introLeadInStyle,
+  box
+} from "../stylesheets/components/Hero.module.sass";
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1)
-  },
-  input: {
-    display: "none"
-  }
-}));
-
-const Hero = ({
-  imageLink,
-  imageAlt,
-  title,
-  subtitle,
-  button1Text,
-  button1Link,
-  button2Text,
-  button2Link
-}) => {
-  const classes = useStyles();
-
+const Hero = ({ introHeading, introLeadIn, resumeButtonText, resumeLink }) => {
   return (
-    <div className="row">
-      <div className="col-md-2" />
-      <div className="col-md-4">
-        <img
-          className="col-md-12 rounded-circle"
-          src={imageLink}
-          alt={imageAlt}
-        />
+    <header>
+      <div className={`${introMargin}`}>
+        <div className={`${introHeadingStyle} ${intro}`}>{introHeading}</div>
+        <div className={`${introLeadInStyle} ${intro}`}>{introLeadIn}</div>
+        <a href={resumeLink} className={box}>
+          {resumeButtonText}
+        </a>
       </div>
-      <div className="col-md-1" />
-      <div className="col-md-5 align-self-center">
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <div className="" />
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          href={button1Link}
-        >
-          {button1Text}
-        </Button>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          href={button2Link}
-        >
-          {button2Text}
-        </Button>
-      </div>
-    </div>
+    </header>
   );
 };
 
 Hero.propTypes = {
-  imageLink: PropTypes.string,
-  imageAlt: PropTypes.string,
-  title: PropTypes.string,
-  subtitle: PropTypes.string,
-  button1Text: PropTypes.string,
-  button1Link: PropTypes.string,
-  button2Text: PropTypes.string,
-  button2Link: PropTypes.string
-};
-
-Hero.defaultProps = {
-  imageLink: null,
-  imageAlt: null,
-  title: null,
-  subtitle: null,
-  button1Text: null,
-  button1Link: null,
-  button2Text: null,
-  button2Link: null
+  introHeading: PropTypes.string.isRequired,
+  introLeadIn: PropTypes.string.isRequired,
+  resumeButtonText: PropTypes.string.isRequired,
+  resumeLink: PropTypes.string.isRequired
 };
 
 export default Hero;
