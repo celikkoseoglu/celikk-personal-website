5a069829c0c50242942afff203acb875fe1701b9
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-11-02 20:36:19 +0000

    found out a way to retrieve file content and added some styling to the page

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../index.sass";
 import "bootstrap/dist/css/bootstrap.css";
 import Markdown from "markdown-to-jsx";
+import Container from "react-bootstrap/Container";
+
+import {
+  blogPostImage,
+  blogPostBackground
+} from "../stylesheets/BlogPost.module.sass";
+
+const fooBlog = require("../blog/foo.md");
 
 const BlogPost = () => {
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    fetch(fooBlog)
+      .then(res => res.text())
+      .then(response => setPost(response))
+      .catch(err => setPost(err));
+  });
+
   return (
-    <div className="container">
-      <Markdown># Hello world!</Markdown>
-    </div>
+    <React.Fragment>
+      <div className={blogPostImage} />
+      <Container className={blogPostBackground}>
+        <Markdown>{post}</Markdown>
+      </Container>
+    </React.Fragment>
   );
 };
 
