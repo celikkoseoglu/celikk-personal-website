1ab270990baa0940a3f2b2ef2f5cae61ed89a029
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-11-03 17:44:57 +0000

    project cards clickable

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import {
   titleMargin,
-  subtitleMargin
+  subtitleMargin,
+  point
 } from "../stylesheets/components/ProjectCard.module.sass";
 import SpringDiv from "./Animations/SpringDiv";
 
-const ProjectCard = ({
-  imageLink,
-  imageAlt,
-  title,
-  subtitle,
-  text,
-  blogPost
-}) => {
-  return (
-    <SpringDiv className="col-md-4 text-center">
-      <img src={imageLink} className="img-fluid" alt={imageAlt} />
-      <h4 className={titleMargin}>{title}</h4>
-      <h6 className={subtitleMargin}>{subtitle}</h6>
-      <p>{text}</p>
-      <Link to={`/blog/${blogPost}`}>Click to read the Blog Post</Link>
-    </SpringDiv>
-  );
-};
+// withRouter is a higher-order component that allows this component to be clickable and it navigates to another page!
+// https://stackoverflow.com/questions/31079081/programmatically-navigate-using-react-router
+
+const ProjectCard = withRouter(
+  ({ history, imageLink, imageAlt, title, subtitle, text, blogPost }) => {
+    return (
+      <SpringDiv
+        onClick={() => {
+          history.push(`/blog/${blogPost}`);
+        }}
+        className={`col-md-4 text-center ${point}`}
+      >
+        <img src={imageLink} className="img-fluid" alt={imageAlt} />
+        <h4 className={titleMargin}>{title}</h4>
+        <h6 className={subtitleMargin}>{subtitle}</h6>
+        <p>{text}</p>
+      </SpringDiv>
+    );
+  }
+);
 
 ProjectCard.propTypes = {
   imageLink: PropTypes.string,
