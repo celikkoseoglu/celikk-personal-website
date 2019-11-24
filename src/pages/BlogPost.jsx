4eaf19926d22411f1e1edfb85488a2e2e9fd5361
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-11-24 18:32:30 +0000

    added goBack to scroll position to Blog post

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -4,7 +4,8 @@ import Markdown from "markdown-to-jsx";
 import Container from "react-bootstrap/Container";
 
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import { blogPostBackground } from "../stylesheets/BlogPost.module.sass";
 import { footerBackground, footerPadding } from "../stylesheets/Home.module.sass";
 import Footer from "../components/Footer/Footer";
@@ -13,21 +14,33 @@ import Signature from "../data/images/signature.svg";
 const blogNavbar = require("../data/blogNavbar");
 const footer = require("../data/footer");
 
-const BlogPost = ({ match }) => {
+const BlogPost = ({ match, history }) => {
   const [post, setPost] = useState("");
+  const [hasHistory, setHasHistory] = useState(false);
 
   useEffect(() => {
+    if (history.length > 1) {
+      setHasHistory(true);
+    }
+
     window.scrollTo(0, 0);
     fetch(`/static/media/${match.params.blogPost}`)
       .then(res => res.text())
       .then(response => setPost(response))
       .catch(err => setPost(err));
-  }, [match.params.blogPost]);
+  }, [match.params.blogPost, history.length]);
 
   return (
     <React.Fragment>
       <Container className={`p-4 rounded-top ${blogPostBackground}`}>
-        <Link to={blogNavbar.heroLink}>{blogNavbar.heroTitle}</Link>
+        {hasHistory ? (
+          <Button onClick={() => (history.goBack() ? undefined : history.goForward())}>
+            {blogNavbar.goBackLabel}
+          </Button>
+        ) : (
+          <Link to={blogNavbar.homeLink}>{blogNavbar.blogLabel}</Link>
+        )}
+
         <Markdown>{post}</Markdown>
       </Container>
 
@@ -45,7 +58,12 @@ BlogPost.propTypes = {
     params: PropTypes.shape({
       blogPost: PropTypes.string.isRequired
     })
+  }).isRequired,
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+    goForward: PropTypes.func.isRequired,
+    length: PropTypes.number.isRequired
   }).isRequired
 };
 
-export default BlogPost;
+export default withRouter(BlogPost);
