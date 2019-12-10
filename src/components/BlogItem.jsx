debada14c9630c42143ca842f599407bffe059ed
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-12-10 20:56:19 +0000

    blog styling updates and more hero images

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import UnstyledLink from "./Util/UnstyledLink";
+import { blogTitle, blogSubtitle } from "../stylesheets/components/BlogItem.module.sass";
 
 const BlogItem = ({ className, title, date, readingLength, subtitle, blogPost }) => {
   return (
     <div className={className}>
-      <UnstyledLink to={`/blog/${blogPost}`}>
+      <UnstyledLink to={`/blog/${blogPost}`} className={`${blogTitle} ${blogSubtitle}`}>
         <h4>{title}</h4>
+        <h6>
+          {date} - {readingLength}
+        </h6>
       </UnstyledLink>
-      <h6>
-        {date} - {readingLength}
-      </h6>
       <p>{subtitle}</p>
     </div>
   );
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,40 +3,44 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Container, Row } from "react-bootstrap";
 import BlogItem from "../components/BlogItem";
 
+import { profilePicture, background, blogTitleFont } from "../stylesheets/Blog.module.sass";
+
 const blog = require("../data/blog");
 
 const Blog = () => {
   return (
-    <Container className="pt-md-5 py-2">
-      <div className="col-md-8 mx-auto">
-        <h1 className="pb-md-5">{blog.name}</h1>
-        <Row className="pb-md-5 pb-2">
-          <div className="col-2 my-auto pr-0">
-            <img
-              className="img-fluid rounded-circle"
-              src={`${blog.imageLink}`}
-              alt={blog.imageAlt}
-            />
-          </div>
-          <div className="col-10 my-auto">
-            <p className="my-auto py-md-1">{blog.title}</p>
-            <p className="my-auto py-md-1">{blog.subtitle}</p>
-          </div>
-        </Row>
+    <body className={background}>
+      <Container className="pt-md-5 py-2">
+        <div className="col-md-8 mx-auto">
+          <h1 className="pb-md-5 py-4 pt-md-0">{blog.name}</h1>
+          <Row className="pb-md-5 pb-5 col-12">
+            <div className="my-auto">
+              <img
+                className={`img-fluid rounded-circle ${profilePicture}`}
+                src={`${blog.imageLink}`}
+                alt={blog.imageAlt}
+              />
+            </div>
+            <div className="my-auto">
+              <p className={`my-auto py-md-1 ${blogTitleFont}`}>{blog.title}</p>
+              <p className={`my-auto py-md-1 ${blogTitleFont}`}>{blog.subtitle}</p>
+            </div>
+          </Row>
 
-        {blog.blogItems.map(blogItem => (
-          <BlogItem
-            className="pb-md-4"
-            title={blogItem.title}
-            date={blogItem.date}
-            readingLength={blogItem.readingLength}
-            subtitle={blogItem.subtitle}
-            blogPost={blogItem.blogPost}
-            key={blogItem.title}
-          />
-        ))}
-      </div>
-    </Container>
+          {blog.blogItems.map(blogItem => (
+            <BlogItem
+              className="pb-md-4 pb-3"
+              title={blogItem.title}
+              date={blogItem.date}
+              readingLength={blogItem.readingLength}
+              subtitle={blogItem.subtitle}
+              blogPost={blogItem.blogPost}
+              key={blogItem.title}
+            />
+          ))}
+        </div>
+      </Container>
+    </body>
   );
 };
 
