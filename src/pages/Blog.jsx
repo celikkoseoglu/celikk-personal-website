b8838e874c380e6f2a1d4b97a1c892ee42a69928
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-12-10 21:16:43 +0000

    added footer to blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,16 +4,21 @@ import { Container, Row } from "react-bootstrap";
 import BlogItem from "../components/BlogItem";
 
 import { profilePicture, background, blogTitleFont } from "../stylesheets/Blog.module.sass";
+import Signature from "../data/images/signature.svg";
+import { signature } from "../stylesheets/components/Footer.module.sass";
+import SocialMediaBar from "../components/Footer/SocialMediaBar";
+import { socialMediaButtonBackground } from "../stylesheets/BlogPost.module.sass";
 
 const blog = require("../data/blog");
+const footer = require("../data/footer");
 
 const Blog = () => {
   return (
     <body className={background}>
-      <Container className="pt-md-5 py-2">
-        <div className="col-md-8 mx-auto">
+      <Container className="pt-md-5 py-4">
+        <div className="col-md-8 mx-auto px-0">
           <h1 className="pb-md-5 py-4 pt-md-0">{blog.name}</h1>
-          <Row className="pb-md-5 pb-5 col-12">
+          <Row className="pb-md-5 pb-5 col-12 pr-0">
             <div className="my-auto">
               <img
                 className={`img-fluid rounded-circle ${profilePicture}`}
@@ -38,6 +43,25 @@ const Blog = () => {
               key={blogItem.title}
             />
           ))}
+
+          <hr />
+          <Row className="text-center">
+            <div className="col-md-4 pb-md-0 pb-3">
+              <span>{footer.title}</span>
+              <br />
+              <img
+                src={Signature}
+                alt="signature"
+                className={`img-responsive img-centered ${signature}`}
+              />
+            </div>
+            <div className="col-md-8 my-auto px-0">
+              <SocialMediaBar
+                socialMediaLinks={footer.socialMediaLinks}
+                buttonBackground={socialMediaButtonBackground}
+              />
+            </div>
+          </Row>
         </div>
       </Container>
     </body>
