ff95a0bbcba8004f173834ea9052144fcbc9acd8
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-12-18 23:37:01 +0000

    implemented webp fallback strategy for the rest of the website

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {
   titleMargin,
@@ -8,12 +8,21 @@ import {
 } from "../stylesheets/components/ProjectCard.module.sass";
 import SpringDiv from "./Animations/SpringDiv";
 import UnstyledLink from "./Util/UnstyledLink";
+import { getImageLinkWithExtension } from "../utils/FileManager.utils";
 
 const ProjectCard = ({ imageLink, imageAlt, title, subtitle, text, blogPost }) => {
+  const [relativeImageLink, setRelativeImageLink] = useState(null);
+  useEffect(() => {
+    const imageLinkWithExtension = getImageLinkWithExtension(imageLink);
+    import(`../${imageLinkWithExtension}`).then(loadedLink => {
+      setRelativeImageLink(loadedLink.default);
+    });
+  }, [relativeImageLink, imageLink]);
+
   return (
     <SpringDiv className={`col-md-4 text-center ${point} ${mobileSize}`}>
       <UnstyledLink to={`/blog/${blogPost}`}>
-        <img src={`/static/media/${imageLink}`} className="img-fluid" alt={imageAlt} />
+        {relativeImageLink ? <img src={relativeImageLink} className="img-fluid" alt={imageAlt} /> : null}
         <h4 className={titleMargin}>{title}</h4>
         <h6 className={subtitleMargin}>{subtitle}</h6>
         <p>{text}</p>
--- src/utils/FileManager.utils.jsx
+++ src/utils/FileManager.utils.jsx
@@ -5,11 +5,7 @@ const importAll = r => r.keys().map(r);
 export const IMAGE_EXTENSION = isWebpSupported() ? ".webp" : ".png";
 
 export const folders = Object.freeze({
-  ongoingProjectImages: importAll(
-    require.context(`../data/images/ongoingProjects`, false, /\.(png|svg)$/)
-  ),
   personalSkillImages: importAll(require.context(`../data/images/personalSkills`, false, /\.png$/)),
-  projectImages: importAll(require.context(`../data/images/projects`, false, /\.png$/)),
   blogFiles: importAll(require.context("../blog", false, /\.md$/)),
   heroImages: importAll(require.context("../data/images/hero", false, /\.jpg$/))
 });
@@ -21,4 +17,11 @@ export const mapFileNameToId = (fileName, fileArray) => {
     }
   }
   return null;
+};
+
+export const getImageLinkWithExtension = imageLink => {
+  if (!imageLink.endsWith(".svg")) {
+    return imageLink + IMAGE_EXTENSION;
+  }
+  return imageLink;
 };
