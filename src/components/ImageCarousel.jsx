397e38f103b0d89bfe7c16e9f4a54a64457211dc
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-01-05 16:27:27 +0000

    added help text for mobile safari users in blog post image carousel

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -4,12 +4,22 @@ import PropTypes from "prop-types";
 import {
   autoSizeImage,
   horizontalOverflow,
-  imageCarousel
+  imageCarousel,
+  showHelpText
 } from "../stylesheets/components/ImageCarousel.module.sass";
 import { getImageLinkWithExtension } from "../utils/FileManager.utils";
 
 const ImageCarousel = ({ folder, images }) => {
+  // detect if the user is coming from an iOS device and show help text instead of scroll bar
+  // because mobile safari doesn't show scroll bars
+  const iOS =
+    !!window.navigator.userAgent.match(/iPad/i) || !!window.navigator.userAgent.match(/iPhone/i);
+
   const [imageLoaded, setImageLoaded] = useState([]);
+
+  const hasMultipleImagesAndiOS = () => {
+    return (iOS && imageLoaded.length > 1);
+  };
   useEffect(() => {
     const imageLinkWithExtension = imageFileName => {
       return `data/images/blog/${folder}/${getImageLinkWithExtension(imageFileName)}`;
@@ -32,18 +42,30 @@ const ImageCarousel = ({ folder, images }) => {
   };
 
   return (
-    <div align="center" className={`${horizontalOverflow} ${imageCarousel} py-3 my-3`}>
-      {imageLoaded
-        ? imageLoaded.map((imageRelativeLink, index) => (
-            <img
-              src={imageRelativeLink}
-              className={`${autoSizeImage} ${imageSpacer(index, imageLoaded.length)}`}
-              alt={imageRelativeLink}
-              key={imageRelativeLink}
-            />
-          ))
-        : null}
-    </div>
+    <>
+      <div
+        align="center"
+        className={`${horizontalOverflow} ${imageCarousel} ${
+          hasMultipleImagesAndiOS() ? "pt-3 pb-0 mt-3 mb-1" : "py-3 my-3"
+        }`}
+      >
+        {imageLoaded
+          ? imageLoaded.map((imageRelativeLink, index) => (
+              <img
+                src={imageRelativeLink}
+                className={`${autoSizeImage} ${imageSpacer(index, imageLoaded.length)}`}
+                alt={imageRelativeLink}
+                key={imageRelativeLink}
+              />
+            ))
+          : null}
+      </div>
+      {hasMultipleImagesAndiOS() ? (
+        <span className={`d-flex justify-content-center pb-3 ${showHelpText}`}>
+          Swipe to see all images
+        </span>
+      ) : null}
+    </>
   );
 };
 
