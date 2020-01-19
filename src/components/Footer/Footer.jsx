311cd5d54dad598c07eb6e3e450af37504c14256
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-01-19 01:51:24 +0000

    finalized footer work

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,71 +2,59 @@ import React from "react";
 import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import {
-  linkColor,
+  footerDarkText,
+  footerLightText,
   leftText,
-  borderRight,
-  name
+  lineSeparator,
+  footerName,
+  footerList
 } from "../../stylesheets/components/Footer/Footer.module.sass";
 
 import SocialMediaBar from "./SocialMediaBar";
 import Signature from "../Signature";
-import HorizontalRuler from "./HorizontalRuler";
 
 const Footer = ({ content, signatureImage }) => {
   return (
-    <footer className="col-md-12 px-0">
+    <footer className="col-md-12 px-0 pt-md-2">
       <Row>
-        <div className={`col-md-6 ${borderRight}`}>
-          <p className={leftText}>LET&apos;S MEET</p>
+        <div className={`col-md-6 ${lineSeparator} pr-md-5 pb-3 pb-md-0 mx-3 mx-md-0`}>
+          <p
+            className={`${leftText} ${footerLightText}
+             h-100 d-flex justify-content-center flex-column text-center text-md-right`}
+          >
+            {content.leftText}
+          </p>
         </div>
 
-        <div className="col-md-6">
-          <p className={name}>Celik Koseoglu</p>
-          <p>London, UK</p>
+        <div className="pl-md-5 col-md-6 my-auto text-center text-md-left pt-3 pt-md-0">
+          <p className={`${footerLightText} ${footerName} mb-1`}>{content.title}</p>
+          <p className={`${footerLightText} mb-3`}>London, UK</p>
           <SocialMediaBar socialMediaLinks={content.socialMediaLinks} />
-          <HorizontalRuler isDark />
-
-          <div>
-            <p>I&apos;ve streamed myself making this website. Here are some videos</p>
-            <ul className="pl-3">
+          <div className="pt-4">
+            <p className={`mb-1 ${footerLightText}`}>{content.linkText}</p>
+            <ul className={`pl-md-2 pl-0 ${footerLightText} ${footerList} mb-0`}>
               <li>
-                <a href={content.link} className={linkColor}>
-                  Making the Navbar
+                <a href={content.link1} className={footerDarkText}>
+                  {content.text1}
                 </a>
               </li>
               <li>
-                <a href={content.link} className={linkColor}>
-                  Creating the Footer
+                <a href={content.link2} className={footerDarkText}>
+                  {content.text2}
                 </a>
               </li>
               <li>
-                <a href={content.link} className={linkColor}>
-                  Dark Mode for Blog
+                <a href={content.link3} className={footerDarkText}>
+                  {content.text2}
                 </a>
               </li>
             </ul>
           </div>
-
-          <Signature signatureImage={signatureImage} />
         </div>
-
-        {/* <div className="col-md-3 col-lg-4"> */}
-        {/*  <span>{content.title}</span> */}
-        {/*  <div className="pt-md-3 pb-md-0 py-2"> */}
-
-        {/*    </a> */}
-        {/*  </div> */}
-        {/* </div> */}
-
-        {/* <div className="col-md-6 col-lg-4 my-auto px-0"> */}
-
-        {/* </div> */}
-
-        {/* <div className="col-md-3 col-lg-4"> */}
-        {/*  <span>{content.date}</span> */}
-
-        {/* </div> */}
       </Row>
+
+      <Signature className="mt-4 mb-2" signatureImage={signatureImage} isDark />
+      <p className={`text-center mb-0 ${footerDarkText}`}>{content.date}</p>
     </footer>
   );
 };
@@ -74,8 +62,14 @@ const Footer = ({ content, signatureImage }) => {
 Footer.propTypes = {
   content: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    leftText: PropTypes.string.isRequired,
     linkText: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
+    link1: PropTypes.string.isRequired,
+    link2: PropTypes.string.isRequired,
+    link3: PropTypes.string.isRequired,
+    text1: PropTypes.string.isRequired,
+    text2: PropTypes.string.isRequired,
+    text3: PropTypes.string.isRequired,
     socialMediaLinks: PropTypes.shape({
       twitterLink: PropTypes.string.isRequired,
       facebookLink: PropTypes.string.isRequired,
