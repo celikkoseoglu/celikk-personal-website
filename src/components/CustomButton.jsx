53bb782f50a26f05b8de13c263b1e4c606f05b35
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-03-23 23:20:06 +0000

    improved design for 404 page

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import { customButton, customButtonDark } from "../stylesheets/components/CustomButton.module.sass";
 import UnstyledLink from "./Util/UnstyledLink";
 
-const CustomButton = ({ text, to, href, isDark }) => {
+const CustomButton = ({ text, to, href, isDark, className }) => {
   return (
     <UnstyledLink
       to={to}
       href={href}
-      className={`text-center ${customButton} ${isDark ? customButtonDark : null}`}
+      className={`text-center ${customButton} ${className} ${isDark ? customButtonDark : null}`}
     >
       {text}
     </UnstyledLink>
@@ -19,13 +19,15 @@ CustomButton.propTypes = {
   text: PropTypes.string.isRequired,
   to: PropTypes.string,
   href: PropTypes.string,
-  isDark: PropTypes.bool
+  isDark: PropTypes.bool,
+  className: PropTypes.string
 };
 
 CustomButton.defaultProps = {
   to: null,
   href: null,
-  isDark: false
+  isDark: false,
+  className: null
 };
 
 export default CustomButton;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "../index.sass";
+import CustomButton from "../components/CustomButton";
+import { notFound, customButton } from "../stylesheets/NotFound.module.sass";
 
 const NotFound = () => {
-  return <p>404</p>;
+  return (
+    <div className={notFound}>
+      <div>
+        <h1>404</h1>
+        <p>The page you’re looking for is not there</p>
+        <CustomButton className={customButton} text="Home" to="/" href="/" />
+        <CustomButton className={customButton} text="Blog" to="/blog" href="/blog" />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
