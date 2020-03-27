c5f418eb72b439e51343001fd755e3da084c6486
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-03-27 18:55:05 +0000

    merged ongoing projects with projects and improved skills styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import ProgressiveImage from "react-progressive-image";
 import Container from "react-bootstrap/Container";
-import { Row } from "react-bootstrap";
+import {Row} from "react-bootstrap";
 import Hero from "../components/Hero";
 import CenteredText from "../components/CenteredText";
 import ImageExperience from "../components/ImageExperience";
 import ProjectCard from "../components/ProjectCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
-  whyDeveloperBackground,
-  skillsBackground,
-  projectsBackground,
-  ongoingProjectsBackground,
-  latestBlogPostsBackground,
-  footerBackground,
-  sectionPadding,
-  footerPadding,
-  paddingBottom,
   arrowMargin,
   arrowSize,
+  blogShowcaseContainer,
+  footerBackground,
+  footerPadding,
   heroBackground,
-  blogShowcaseContainer
+  latestBlogPostsBackground,
+  paddingBottom,
+  projectsBackground,
+  sectionPadding,
+  skillsBackground,
+  whyDeveloperBackground
 } from "../stylesheets/Home.module.sass";
 import Footer from "../components/Footer/Footer";
 import Heading from "../components/Heading";
 import NavigationBar from "../components/Navbar/NavigationBar";
 import Signature from "../data/images/signature.svg";
 import ArrowAnimation from "../components/Animations/ArrowAnimation";
-import { folders, getRandomInt } from "../utils/FileManager.utils";
+import {folders, getRandomInt} from "../utils/FileManager.utils";
 import BlogShowcaseCard from "../components/BlogShowcase/BlogShowcaseCard";
-import { BLOG_LINK } from "../utils/Constants.utils";
+import {BLOG_LINK} from "../utils/Constants.utils";
 import BlogShowcaseButton from "../components/BlogShowcase/BlogShowcaseButton";
 import {
   NUMBER_OF_LATEST_BLOG_CARDS_TO_RENDER_ON_MOBILE,
@@ -40,7 +39,6 @@ const navbar = require("../data/navbar");
 const content = require("../data/content");
 const hero = require("../data/hero");
 const personalSkills = require("../data/personalSkills");
-const ongoingProjects = require("../data/ongoingProjects");
 const projects = require("../data/projects");
 const footer = require("../data/footer");
 const blog = require("../data/blog");
@@ -111,38 +109,14 @@ const Home = () => {
 
       <section id={content.skillsReference} className={`${skillsBackground} ${sectionPadding}`}>
         <Heading className={paddingBottom} text={content.skillsTitle} />
-        <div>
-          {personalSkills.map(personalSkill => (
-            <ImageExperience
-              imageLink={personalSkill.imageLink}
-              imageAlt={personalSkill.imageAlt}
-              text={personalSkill.text}
-              key={personalSkill.imageAlt}
-            />
-          ))}
-        </div>
-      </section>
-
-      <section
-        id={content.ongoingProjectsReference}
-        className={`${ongoingProjectsBackground} ${sectionPadding}`}
-      >
-        <Heading className={paddingBottom} text={content.ongoingProjectsTitle} />
-        <Container>
-          <Row>
-            {ongoingProjects.map(project => (
-              <ProjectCard
-                imageLink={project.imageLink}
-                imageAlt={project.imageAlt}
-                title={project.title}
-                subtitle={project.subtitle}
-                text={project.text}
-                blogPost={project.blogPost}
-                key={project.title}
-              />
-            ))}
-          </Row>
-        </Container>
+        {personalSkills.map(personalSkill => (
+          <ImageExperience
+            imageLink={personalSkill.imageLink}
+            imageAlt={personalSkill.imageAlt}
+            text={personalSkill.text}
+            key={personalSkill.imageAlt}
+          />
+        ))}
       </section>
 
       <section
