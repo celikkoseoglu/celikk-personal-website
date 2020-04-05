286b34aeb222604dfb53771173bd1751ae510758
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-04-05 01:19:44 +0100

    Fix DarkModeToggle button animation glitch for mobile safari 13.4

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { sun, moon, darkModeToggle } from "../stylesheets/components/DarkModeToggle.module.sass";
+import {
+  sun,
+  moon,
+  darkModeToggle,
+  crescent,
+} from "../stylesheets/components/DarkModeToggle.module.sass";
 
 const DarkModeToggle = ({ isDark, onClickMethod }) => {
   return (
     <button
       type="button"
-      onClick={_ => onClickMethod(!isDark)}
+      aria-label="Dark Mode Toggle"
+      onClick={(_) => onClickMethod(!isDark)}
       className={`${isDark ? moon : sun} my-auto ${darkModeToggle}`}
     >
-      Dark Mode Toggle
+      <div className={crescent} />
     </button>
   );
 };
 
 DarkModeToggle.propTypes = {
   isDark: PropTypes.bool.isRequired,
-  onClickMethod: PropTypes.func.isRequired
+  onClickMethod: PropTypes.func.isRequired,
 };
 
 export default DarkModeToggle;
