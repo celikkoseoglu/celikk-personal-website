2481c6563970f654ae36041cb5467a436a590896
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-04-09 23:06:33 +0100

    Remove bootstrap from CV component

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Experience from "../components/CV/Experience/Experience";
 import Skill from "../components/CV/Skill";
-import "../index.sass";
+import Container from "../components/Util/Container";
+import Row from "../components/Util/Row";
 
 const experiences = require("../data/resume/experiences");
 const resumeSkills = require("../data/resume/skills");
 
 const CV = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
+    <Container>
+      <Row>
+        <div>
           {resumeSkills.map(skill => (
             <Skill header={skill.header} content={skill.content} />
           ))}
         </div>
-
-        <div className="col-md-8">
+        <div>
           {experiences.map(experience => (
             <Experience
               companyName={experience.companyName}
@@ -25,8 +25,8 @@ const CV = () => {
             />
           ))}
         </div>
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 };
 
