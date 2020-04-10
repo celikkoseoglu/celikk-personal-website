723fd58741298550b984cdbe17322345459c3a5e
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-04-10 23:05:47 +0100

    Remove bootstrap from BlogShowcaseCard

--- src/components/BlogShowcase/BlogShowcaseCard.jsx
+++ src/components/BlogShowcase/BlogShowcaseCard.jsx
@@ -5,7 +5,7 @@ import {
   montserrat,
   timestampStyle,
   titleStyle,
-  description
+  description,
 } from "../../stylesheets/components/BlogShowcase/BlogShowcaseCard.module.sass";
 import UnstyledLink from "../Util/UnstyledLink";
 import { BLOG_LINK } from "../../utils/Constants.utils";
@@ -15,7 +15,7 @@ const BlogShowcaseCard = ({ title, subtitle, timestamp, minutes, blogPost, class
     <div className={`${square} ${className}`}>
       <UnstyledLink to={`${BLOG_LINK}${blogPost}`}>
         <div className={description}>
-          <h6 className={`${montserrat} ${titleStyle} pb-1`}>{title}</h6>
+          <h6 className={`${montserrat} ${titleStyle}`}>{title}</h6>
           <p>{subtitle}</p>
         </div>
         <p className={`${timestampStyle} ${montserrat}`}>
@@ -32,11 +32,11 @@ BlogShowcaseCard.propTypes = {
   timestamp: PropTypes.string.isRequired,
   minutes: PropTypes.string.isRequired,
   blogPost: PropTypes.string.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 BlogShowcaseCard.defaultProps = {
-  className: null
+  className: null,
 };
 
 export default BlogShowcaseCard;
