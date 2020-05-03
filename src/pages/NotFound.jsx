9ad62a4a78fd92c2471243efb34998162f1080f3
Celik Koseoglu <celikkoseoglu@yahoo.com>
2020-05-03 18:19:29 +0100

    Fix 404 page paddings for small screen horizontal devices

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "../index.sass";
 import CustomButton from "../components/CustomButton";
-import { notFound, customButton } from "../stylesheets/NotFound.module.sass";
+import { notFound, customButton, noMargin } from "../stylesheets/NotFound.module.sass";
 import { firebaseAnalytics } from "../firebaseConfig";
 
 const NotFound = () => {
@@ -12,7 +12,7 @@ const NotFound = () => {
   return (
     <div className={notFound}>
       <div>
-        <h1>404</h1>
+        <h1 className={noMargin}>404</h1>
         <p>The page you’re looking for is not there</p>
         <CustomButton className={customButton} text="Home" to="/" href="/" />
         <CustomButton className={customButton} text="Blog" to="/blog" href="/blog" />
