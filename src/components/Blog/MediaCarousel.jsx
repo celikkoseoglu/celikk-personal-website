c48fa230ec794f04aac2d63912e50db96354efc7
Çelik Köseoğlu <celikkoseoglu@yahoo.com>
2021-01-03 00:38:25 +0000

    use require instead of dynamic import in MediaCarousel

--- src/components/Blog/MediaCarousel.jsx
+++ src/components/Blog/MediaCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {
   autoSizeImage,
@@ -9,7 +9,7 @@ import {
   mediaCarousel,
   multipleImage,
 } from "../../stylesheets/components/Blog/MediaCarousel.module.sass";
-import {IS_WEBP_SUPPORTED} from "../../utils/Constants.utils";
+import { IS_WEBP_SUPPORTED } from "../../utils/Constants.utils";
 
 const getImageLinkWithExtension = (imageLink) => {
   if (IS_WEBP_SUPPORTED && imageLink.endsWith(".png")) {
@@ -18,66 +18,54 @@ const getImageLinkWithExtension = (imageLink) => {
   return imageLink;
 };
 
-const imageLinkWithExtension = (folder, imageFileName) => {
-  return `data/images/blogPost/${folder}/${getImageLinkWithExtension(imageFileName)}`;
-};
-
 const MediaCarousel = ({ folder, images, isDark }) => {
   const imageFileNames = images.split(",");
-  const [imageLoaded, setImageLoaded] = useState(new Array(imageFileNames.length));
+  const imagesToBeLoaded = [];
 
-  // detect if the user is coming from an iOS device and show help text instead of scroll bar
-  // because mobile safari doesn't show scroll bars
-  useEffect(() => {
-    imageFileNames.map((imageFileName, index) =>
-      import(`../../${imageLinkWithExtension(folder, imageFileName)}`).then((imageLink) => {
-        setImageLoaded((oldArray) => {
-          const prev = [...oldArray];
-          prev[index] = imageLink.default;
-          return prev;
-        });
-      })
+  for (let i = 0; i < imageFileNames.length; i += 1) {
+    imagesToBeLoaded.push(
+      // eslint-disable-next-line global-require,import/no-dynamic-require
+      require(`../../data/images/blogPost/${folder}/${getImageLinkWithExtension(
+        imageFileNames[i]
+      )}`)
     );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [folder, images]);
+  }
 
   return (
     <>
       <div
         align="center"
         className={`${horizontalOverflow} ${isDark ? darkMediaCarousel : null} ${mediaCarousel} ${
-          imageLoaded.length > 1 && multipleImage
+          imagesToBeLoaded.length > 1 && multipleImage
         }`}
       >
-        {imageLoaded
-          ? imageLoaded.map((imageRelativeLink, index) =>
-              imageRelativeLink !== undefined && imageRelativeLink.endsWith(".mp4") ? (
-                // eslint-disable-next-line jsx-a11y/media-has-caption
-                <video
-                  className={`${autoSizeImage} ${imageLoaded.length > 1 && autoSizeMultipleImage} ${
-                    index > 0 && imageMargin
-                  }`}
-                  loop
-                  autoPlay
-                  playsInline
-                  muted
-                  key={imageRelativeLink}
-                >
-                  <source src={imageRelativeLink} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              ) : (
-                <img
-                  src={imageRelativeLink}
-                  className={`${autoSizeImage} ${imageLoaded.length > 1 && autoSizeMultipleImage} ${
-                    index > 0 && imageMargin
-                  }`}
-                  alt={imageRelativeLink}
-                  key={imageRelativeLink}
-                />
-              )
-            )
-          : null}
+        {imagesToBeLoaded.map((imageRelativeLink, index) =>
+          imageRelativeLink !== undefined && imageRelativeLink.endsWith(".mp4") ? (
+            // eslint-disable-next-line jsx-a11y/media-has-caption
+            <video
+              className={`${autoSizeImage} ${
+                imagesToBeLoaded.length > 1 && autoSizeMultipleImage
+              } ${index > 0 && imageMargin}`}
+              loop
+              autoPlay
+              playsInline
+              muted
+              key={imageRelativeLink}
+            >
+              <source src={imageRelativeLink} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <img
+              src={imageRelativeLink}
+              className={`${autoSizeImage} ${
+                imagesToBeLoaded.length > 1 && autoSizeMultipleImage
+              } ${index > 0 && imageMargin}`}
+              alt={imageRelativeLink}
+              key={imageRelativeLink}
+            />
+          )
+        )}
       </div>
     </>
   );
