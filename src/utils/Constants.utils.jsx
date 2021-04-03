3994bf3d9a1809a28a1067e4ce2a3443074648e2
Çelik Köseoğlu <celikkoseoglu@yahoo.com>
2021-04-04 00:51:35 +0100

    update iPad user agent check to support iOS 13+

--- src/utils/Constants.utils.jsx
+++ src/utils/Constants.utils.jsx
@@ -1,7 +1,16 @@
-import {isWebpSupported} from "react-image-webp/dist/utils";
+import { isWebpSupported } from "react-image-webp/dist/utils";
 
 export const BLOG_LINK = "/blog/";
-export const iPad = !!window.navigator.userAgent.match(/iPad/i);
+
+export const iPad =
+  !!window.navigator.userAgent.match(/iPad/i) ||
+  /* With iPadOS, mobile safari requests desktop websites by default by using the MacOS Safari user
+   agent string. The current workaround is to check if it's a MacOS Safari UserAgent + if it
+    supports more than 0 touch points (touch screen). This might cause issues in the future but
+    it is the only known workaround for now. https://developer.apple.com/forums/thread/119186 */
+  (!!window.navigator.userAgent.match(/Macintosh; Intel Mac OS X/i) &&
+    navigator.maxTouchPoints > 0);
+
 export const iPhone = !!window.navigator.userAgent.match(/iPhone/i);
 export const iOS = iPad || iPhone;
 export const IS_WEBP_SUPPORTED = isWebpSupported();
